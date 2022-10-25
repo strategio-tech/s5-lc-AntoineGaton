4fb77cf97493c0f669395c4cca83a19bd4407cb0
Antoine Gaton <antoinegaton@gmail.com>
2022-10-24 22:16:00 -0400

    built method

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -11,15 +11,28 @@ public class LoanCalculator {
      * @param ???
      * @return ???
      */
-    static int getRemainingAmountIn3Months(int amount) {
+    static void getRemainingAmountIn3Months(int amount, int months) {
         // TODO: Rewrite this method
-        return -1;
+        int remaining = amount;
+        for (int i=1;i<=months;i++){
+            double payment = 0.1 * amount;
+            System.out.println("Month "+i+":");
+            System.out.println("Payment: 10% of "+amount+" = " + (int)payment);
+            amount = (int) (amount - payment);
+            System.out.println("Remaining amount: " + amount);
+            System.out.println("================================");
+        }
     }
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("================================");
+            System.out.print("Enter an amount: ");
             int amount = scanner.nextInt();
-            System.out.println(getRemainingAmountIn3Months(amount));
+            System.out.print("Enter number of months: ");
+            int months = scanner.nextInt();
+            System.out.println("================================");
+            getRemainingAmountIn3Months(amount, months);
         }
     }
 }
